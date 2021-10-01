b9281142879db1811851c3d14e69904f465ba28c
Alfredo C <alfredo_cardenas@live.com>
2021-10-01 14:25:41 -0500

    Add updated OAUTH code to call slack api, w/created& configed bot

--- testCode01.rb
+++ testCode01.rb
@@ -1,10 +1,15 @@
 require 'http'
 require 'json'
 
-# rc = HTTP.post("https://slack.com/api/api.test")
-rc = HTTP.post("https://slack.com/api/auth.test", params: {
-  token: ENV['SLACK_API_TOKEN_01']
+# rc = HTTP.post("https://slack.com/api/api.test") # test01
+# rc = HTTP.post("https://slack.com/api/auth.test", params: { # test02
+#   token: ENV['SLACK_API_TOKEN_01']
+# })
+rc = HTTP.post("https://slack.com/api/oauth.access", params: {
+  client_id: ENV['SLACK_API_CID_01'],
+  client_secret: ENV['SLACK_API_CSCRT_01']
 })
+# should receive a json response with the access_token and scope
 puts rc
 puts "--- #{rc.class} < #{rc.class.superclass} < #{rc.class.superclass.superclass} ---"
 p rc.public_methods - rc.class.superclass.superclass.public_methods

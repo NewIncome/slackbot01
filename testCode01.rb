47f73c3481308bdf804269f1cb28841303f48522
Alfredo C <alfredo_cardenas@live.com>
2021-10-05 22:52:07 -0500

    Trying with the previous and direct Verification Token Failed connection

--- testCode01.rb
+++ testCode01.rb
@@ -2,13 +2,14 @@ require 'http'
 require 'json'
 
 # rc = HTTP.post("https://slack.com/api/api.test") # test01
-# rc = HTTP.post("https://slack.com/api/auth.test", params: { # test02
-#   token: ENV['SLACK_API_TOKEN_01']
-# })
-rc = HTTP.post("https://slack.com/api/oauth.access", params: {
-  client_id: ENV['SLACK_API_CID_01'],
-  client_secret: ENV['SLACK_API_CSCRT_01']
+# test02
+rc = HTTP.post("https://slack.com/api/auth.test", params: {
+  token: "iuWLnftjVLJLrcnE0FgAN3TT"
 })
+#rc = HTTP.post("https://slack.com/api/oauth.access", params: {
+#  client_id: ENV['SLACK_API_CID_01'],
+#  client_secret: ENV['SLACK_API_CSCRT_01']
+#})
 # should receive a json response with the access_token and scope
 puts rc
 puts "--- #{rc.class} < #{rc.class.superclass} < #{rc.class.superclass.superclass} ---"

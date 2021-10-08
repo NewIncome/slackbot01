725a712c901196f8aa2d1a7494b795e0eebb79fa
Alfredo C <alfredo_cardenas@live.com>
2021-10-07 21:12:39 -0500

    Add code to send a message to each user in the workspace Add .gitignore to not push the .env local file created there is code to send a block_question and bttn commented-out

--- test03_SRB.rb
+++ test03_SRB.rb
@@ -1,9 +1,56 @@
 require 'slack-ruby-bot'
+require './.env'
+# # Just like that not working, asks for Slack API Token
+# class PongBot < SlackRubyBot::Bot
+#   command 'ping' do |client, data, match|
+#     client.say(text: 'pong', channel: data.channel)
+#   end
+# end
+# PongBot.run
 
-class PongBot < SlackRubyBot::Bot
-  command 'ping' do |client, data, match|
-    client.say(text: 'pong', channel: data.channel)
-  end
+Slack.configure do |config|
+  config.token = ENV['SLACK_BOT01_USER_OAUTH_TOKEN']
+  raise 'Missing ENV[SLACK_BOT01_USER_OAUTH_TOKEN]!' unless config.token
 end
 
-PongBot.run
+client = Slack::Web::Client.new
+
+all_members = client.users_list
+all_members.members.each do |member|
+  next unless member.is_bot != true
+
+  # block_question = make_block_question(members)
+  block_question = "This is a test"
+  # client.chat_postMessage(channel: member.id, text: 'Buenos dias!',
+  #                         as_user: true, blocks: block_question.to_json)
+  client.chat_postMessage(channel: member.id, text: 'Buenos dias!', as_user: true)
+end
+
+
+# # block of text with button to send to chat
+# def make_block_question(member)
+#   [
+#     {
+#       "type": 'section',
+#       "text": {
+#         "type": 'mrkdwn',
+#         "text": "Hi #{member.real_name} :wave:"
+#       },
+#     },
+#     {
+#       "type": 'section',
+#       "text": {
+#         "type": 'mrkdwn',
+#         "text": 'To check entry time click the button.'
+#       },
+#       "accessory": {
+#         "type": 'button',
+#         "text": {
+#           "type": 'plain_text',
+#           "text": 'Check-in'
+#         },
+#         "value": member.id.to_s
+#       }
+#     }
+#   ]
+# end

fcf4e5c6d8215e46ecc7361f8acd65af7867d530
Alfredo C <alfredo_cardenas@live.com>
2021-10-07 21:24:31 -0500

    Uncomment block code and send block with bttn to user in team working perfectly!

--- test03_SRB.rb
+++ test03_SRB.rb
@@ -8,6 +8,34 @@ require './.env'
 # end
 # PongBot.run
 
+# block of text with button to send to chat
+def make_block_question(member)
+  [
+    {
+      "type": 'section',
+      "text": {
+        "type": 'mrkdwn',
+        "text": "Hi #{member.real_name} :wave:"
+      },
+    },
+    {
+      "type": 'section',
+      "text": {
+        "type": 'mrkdwn',
+        "text": 'To check entry time click the button.'
+      },
+      "accessory": {
+        "type": 'button',
+        "text": {
+          "type": 'plain_text',
+          "text": 'Check-in'
+        },
+        "value": member.id.to_s
+      }
+    }
+  ]
+end
+
 Slack.configure do |config|
   config.token = ENV['SLACK_BOT01_USER_OAUTH_TOKEN']
   raise 'Missing ENV[SLACK_BOT01_USER_OAUTH_TOKEN]!' unless config.token
@@ -19,38 +47,9 @@ all_members = client.users_list
 all_members.members.each do |member|
   next unless member.is_bot != true
 
-  # block_question = make_block_question(members)
-  block_question = "This is a test"
-  # client.chat_postMessage(channel: member.id, text: 'Buenos dias!',
-  #                         as_user: true, blocks: block_question.to_json)
-  client.chat_postMessage(channel: member.id, text: 'Buenos dias!', as_user: true)
+  block_question = make_block_question(member)
+  client.chat_postMessage(channel: member.id, text: 'Buenos dias!',
+                          as_user: true, blocks: block_question.to_json)
+  # block_question = "This is a test"
+  # client.chat_postMessage(channel: member.id, text: 'Good Morning!!', as_user: true)
 end
-
-
-# # block of text with button to send to chat
-# def make_block_question(member)
-#   [
-#     {
-#       "type": 'section',
-#       "text": {
-#         "type": 'mrkdwn',
-#         "text": "Hi #{member.real_name} :wave:"
-#       },
-#     },
-#     {
-#       "type": 'section',
-#       "text": {
-#         "type": 'mrkdwn',
-#         "text": 'To check entry time click the button.'
-#       },
-#       "accessory": {
-#         "type": 'button',
-#         "text": {
-#           "type": 'plain_text',
-#           "text": 'Check-in'
-#         },
-#         "value": member.id.to_s
-#       }
-#     }
-#   ]
-# end
